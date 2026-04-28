6cdbe303875c3695a26007b22e3d366125ed7b00
Rinashala <rinashalaa777@gmail.com>
2026-04-29 00:05:45 +0200

    small change

--- project/server/server.js
+++ project/server/server.js
@@ -18,6 +18,7 @@ app.use(
   })
 )
 app.use(express.json())
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.get('/api/health', (_req, res) => {
   res.json({ ok: true })
